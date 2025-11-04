cd5a9001eb7a7fea3b9741ad35083317b3f4d516
shimuser <smkeyelove@gmail.com>
2025-11-04 11:22:01 +0900

    fix my site

--- components/hero.tsx
+++ components/hero.tsx
@@ -38,7 +38,7 @@ export function Hero() {
   const defaultInfo = {
     greeting: "안녕하세요,",
     name: "심민근",
-    title: "단국대 학생입니다",
+    title: "단국대 돈통입니다",
     description: "404 서버 또 터지면 안되는 레후",
     profileImage: "/uploads/hero-profile-1761477237286.png",
     backgroundImage: "",
